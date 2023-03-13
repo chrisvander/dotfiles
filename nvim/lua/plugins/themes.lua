e51c1897c517fa57e416c7cc2d4e5cbedb2c8a36
Christian van der Loo <chris.vanderloo@yahoo.com>
2023-03-13 10:22:50 -0400

    changed theme

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -4,7 +4,7 @@ return {
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd([[colorscheme catppuccin]])
+			vim.cmd([[colorscheme catppuccin-mocha]])
 		end,
 	},
 	"olimorris/onedarkpro.nvim",
