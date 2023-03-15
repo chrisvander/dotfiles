be234d2a75551ea4a007accf8dcb4fc3adc479b3
Christian van der Loo <chris.vanderloo@yahoo.com>
2023-03-14 22:31:46 -0400

    added new line and other plugins

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -2,6 +2,7 @@ return {
 	{ "ggandor/lightspeed.nvim", event = "VeryLazy" },
 	{ "tpope/vim-surround", event = "VeryLazy" },
 	{ "stevearc/dressing.nvim", event = "VeryLazy" },
+	{ "j-hui/fidget.nvim", event = "VeryLazy", config = true },
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		event = "VeryLazy",
@@ -76,6 +77,33 @@ return {
 	{
 		"folke/which-key.nvim",
 		config = true,
+	},
+	{
+		"folke/noice.nvim",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
+		opts = {
+			lsp = {
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+				hover = { enabled = false },
+				signature = { enabled = false },
+			},
+			-- you can enable a preset for easier configuration
+			presets = {
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = false, -- add a border to hover docs and signature help
+			},
+		},
 	},
 	{
 		"norcalli/nvim-colorizer.lua",
