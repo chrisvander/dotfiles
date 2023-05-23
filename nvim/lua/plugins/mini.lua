bb31a4ab6fc9658fcbc9ad73e7970dfb9ebb818a
Christian van der Loo <chris.vanderloo@yahoo.com>
2023-05-22 23:38:42 -0400

    tweaked capitalization on session manager

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -6,9 +6,9 @@ return {
       event = "BufReadPre",
       opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" } },
       keys = {
-        { "<leader>qs", function() require("persistence").load() end,                desc = "Restore Session" },
-        { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-        { "<leader>qd", function() require("persistence").stop() end,                desc = "Don't Save Current Session" },
+        { "<leader>qs", function() require("persistence").load() end,                desc = "Restore session" },
+        { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore last session" },
+        { "<leader>qd", function() require("persistence").stop() end,                desc = "Don't save current session" },
       },
     } },
     event = "VimEnter",
