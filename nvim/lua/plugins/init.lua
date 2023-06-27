5addf4aff350938b1bf8db51993d9ee2e8367879
Christian van der Loo <chris.vanderloo@yahoo.com>
2023-06-27 08:58:13 -0400

    stripped out movement plugins for flash.nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,8 +1,6 @@
 return {
-  { "tpope/vim-surround",      event = "VeryLazy" },
-  { "stevearc/dressing.nvim",  event = "VeryLazy" },
-  { "jbyuki/instant.nvim",     event = "VeryLazy" },
-  { "ggandor/lightspeed.nvim", event = "VeryLazy" },
+  { "tpope/vim-surround",     event = "VeryLazy" },
+  { "stevearc/dressing.nvim", event = "VeryLazy" },
   {
     "numToStr/Comment.nvim",
     opts = {
@@ -95,5 +93,47 @@ return {
       },
     },
   },
-
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          -- default options: exact mode, multi window, all directions, with a backdrop
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function()
+          -- show labeled treesitter nodes around the cursor
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          -- jump to a remote location to execute the operator
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "n", "o", "x" },
+        function()
+          -- show labeled treesitter nodes around the search matches
+          require("flash").treesitter_search()
+        end,
+        desc = "Treesitter Search",
+      }
+    },
+  }
 }
