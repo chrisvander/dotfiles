c865b52057430ec12610ffc2c9e3d069dd1138e6
Christian van der Loo <chris.vanderloo@yahoo.com>
2023-07-25 13:54:43 -0400

    simplified configs for commenting

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -88,42 +88,16 @@ return {
     event = "VeryLazy",
     opts = {},
     keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "r", mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+      { "R", mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       {
-        "s",
-        mode = { "n", "x", "o" },
-        function()
-          -- default options: exact mode, multi window, all directions, with a backdrop
-          require("flash").jump()
-        end,
-        desc = "Flash",
+        "<c-s>",
+        mode = { "c" },
+        function() require("flash").toggle() end,
+        desc = "Toggle Flash Search"
       },
-      {
-        "S",
-        mode = { "n", "o", "x" },
-        function()
-          -- show labeled treesitter nodes around the cursor
-          require("flash").treesitter()
-        end,
-        desc = "Flash Treesitter",
-      },
-      {
-        "r",
-        mode = "o",
-        function()
-          -- jump to a remote location to execute the operator
-          require("flash").remote()
-        end,
-        desc = "Remote Flash",
-      },
-      {
-        "R",
-        mode = { "n", "o", "x" },
-        function()
-          -- show labeled treesitter nodes around the search matches
-          require("flash").treesitter_search()
-        end,
-        desc = "Treesitter Search",
-      }
     },
   }
 }
