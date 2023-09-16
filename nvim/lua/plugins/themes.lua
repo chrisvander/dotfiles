c9c1570bb99be1a6cd7535d315e0740f485e5b07
Christian van der Loo <chris.vanderloo@yahoo.com>
2023-09-16 13:15:54 -0400

    switch to ayu

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,14 +1,14 @@
 return {
-	{
-		"catppuccin/nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd([[colorscheme catppuccin-mocha]])
-		end,
-	},
-	--"olimorris/onedarkpro.nvim",
-	--"arzg/vim-colors-xcode",
-	--"rebelot/kanagawa.nvim",
-	--"Shatur/neovim-ayu",
+  {
+    "Shatur/neovim-ayu",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme ayu-mirage]])
+    end,
+  },
+  -- { "olimorris/onedarkpro.nvim", lazy = false, priority = 1000 },
+  --"arzg/vim-colors-xcode",
+  --"rebelot/kanagawa.nvim",
+  --"catppuccin/nvim"
 }
