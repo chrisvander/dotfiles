5d53b40a39086e6eff0a00582f2af714e990c501
Christian van der Loo <chris.vanderloo@yahoo.com>
2023-10-30 09:17:07 -0400

    switch to ayu light

--- nvim/lua/plugins/general.lua
+++ nvim/lua/plugins/general.lua
@@ -13,7 +13,7 @@ return {
       end,
       set_light_mode = function()
         vim.api.nvim_set_option("background", "light")
-        vim.cmd("colorscheme catppuccin-latte")
+        vim.cmd("colorscheme ayu-light")
       end,
     },
   },
