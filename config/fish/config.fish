19c93af8223b9f33970e02e524a1f4653dd36817
Christian van der Loo <chris.vanderloo@icloud.com>
2024-10-02 11:25:22 -0400

    fix: remove reload

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,12 +3,6 @@ set -q EDITOR || set -x EDITOR "nvim"
 set -q VISUAL || set -x VISUAL "nvim"
 set -q MANPAGER || set -x MANPAGER "nvim +Man!"
 
-
-# reload command
-function fish
-  source ~/.config/fish/config.fish
-end
-
 # source brew shellenv
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
