5b71553c41bf032c545426706c58c0b189dca2c2
Christian van der Loo <chris.vanderloo@icloud.com>
2024-10-02 21:48:54 -0400

    added some configuration for fzf bindings & interface

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -17,3 +17,10 @@ starship init fish | source
 
 # zoxide
 zoxide init fish | source
+
+# fzf bindings
+fzf_configure_bindings --directory=\ct --git_log=\cf --git_status=\cg --processes=\cp
+
+# fzf configurations
+set fzf_preview_dir_cmd eza --all --color=always --icons
+set fzf_diff_highlighter delta --paging=never --width=20
