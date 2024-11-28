5335141534323e870befb16ac9aea23c70e58f77
Christian van der Loo <chris.vanderloo@icloud.com>
2024-11-28 15:41:24 -0500

    modify to use $EDITOR

--- config/fish/conf.d/editor_environment.fish
+++ config/fish/conf.d/editor_environment.fish
@@ -1,3 +1,3 @@
 set -q EDITOR || set -x EDITOR nvim
-set -q VISUAL || set -x VISUAL nvim
-set -q MANPAGER || set -x MANPAGER "nvim +Man!"
+set -q VISUAL || set -x VISUAL $EDITOR
+set -q MANPAGER || set -x MANPAGER "$EDITOR +Man!"
