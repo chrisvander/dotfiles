ad968d1141fa1e2ab156b1158cf30ff5881c1c3e
Christian van der Loo <chris.vanderloo@icloud.com>
2025-12-30 09:12:20 -0500

    wrap podman with docker alias

--- config/fish/conf.d/brew.fish
+++ config/fish/conf.d/brew.fish
@@ -1,2 +1,2 @@
 # source brew shellenv
-/opt/homebrew/bin/brew shellenv | source
+/opt/homebrew/bin/brew shellenv 2>/dev/null | source
--- config/fish/functions/k9s.fish
+++
@@ -1,6 +0,0 @@
-function k9s --description 'alias k9s k9s && echo "hi"'
-    if ! aws sts get-caller-identity &>/dev/null
-        aws sso login
-    end
-    command k9s $argv
-end
