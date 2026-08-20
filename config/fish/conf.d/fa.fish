77f5bb283e3002d9c97729586b6dcb1a12f78d90
Christian van der Loo <chris.vanderloo@icloud.com>
2026-08-20 11:36:06 -0400

    remove mise from fa conf

--- config/fish/conf.d/fa.fish
+++ config/fish/conf.d/fa.fish
@@ -1,8 +1,4 @@
-mkdir -p $HOME/.local/state/font-awesome
 set FILE "$HOME/.local/state/font-awesome/faapitoken"
 if [ -f $FILE ]
-    export FA_API_TOKEN=$(cat $FILE)
-else
-    mise x 1password-cli@latest -- op read "op://ci/Font Awesome API Token/password" >$FILE
-    export FA_API_TOKEN=$(cat $FILE)
+  export FA_API_TOKEN=$(cat $FILE)
 end
