a1360f1b56f4003ece9f30dc01cae0fe0d2be672
Chris van der Loo <chris.vanderloo@icloud.com>
2026-09-13 11:11:22 -0400

    add k9s theme

--- modules/home/kubernetes.nix
+++ modules/home/kubernetes.nix
@@ -4,7 +4,54 @@
     kc = "kubectl config get-contexts";
     ks = "kubectl config set-context";
   };
-  programs.k9s.enable = true;
+  programs.k9s = {
+    enable = true;
+    settings.k9s.ui.skin = "terminal";
+    skins.terminal.k9s = {
+      body = {
+        fgColor = "default";
+        bgColor = "default";
+      };
+      prompt.bgColor = "default";
+      info.sectionColor = "default";
+      dialog = {
+        bgColor = "default";
+        labelFgColor = "default";
+        fieldFgColor = "default";
+      };
+      frame = {
+        crumbs.bgColor = "default";
+        title = {
+          bgColor = "default";
+          counterColor = "default";
+        };
+        menu.fgColor = "default";
+      };
+      views = {
+        charts.bgColor = "default";
+        table = {
+          bgColor = "default";
+          header = {
+            fgColor = "default";
+            bgColor = "default";
+          };
+        };
+        xray.bgColor = "default";
+        logs = {
+          bgColor = "default";
+          indicator = {
+            bgColor = "default";
+            toggleOnColor = "default";
+            toggleOffColor = "default";
+          };
+        };
+        yaml = {
+          colonColor = "default";
+          valueColor = "default";
+        };
+      };
+    };
+  };
   home.packages = with pkgs; [
     kind
     kubectl
